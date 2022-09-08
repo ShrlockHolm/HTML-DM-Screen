d718643c83f59e71af082c6e4e0b2e9f02552e75
Bryan Holm <bryan.holm@firefly.com>
2022-09-08 16:44:20 -0500

    Lots of Reformatting

--- scripts.js
+++ scripts.js
@@ -9,10 +9,18 @@ function openPage(pageName, elmnt, color) {
         tablinks[i].style.backgroundColor = "";
         tablinks[i].style.color = "white";
         tablinks[i].style.fontWeight = 'normal'
+        tablinks[i].style.borderColor = "white"
+        tablinks[i].style.borderStyle = "solid"
+        tablinks[i].style.borderWidth = "0px"
+        tablinks[i].style.borderBottomWidth = "2px"
     }
     document.getElementById(pageName).style.display = "block";
     elmnt.style.backgroundColor = 'rgb(182, 182, 182)';
     // elmnt.style.backgroundColor = color;
     elmnt.style.color = "#cc1400"
     elmnt.style.fontWeight = 'bold'
+    elmnt.style.borderWidth = "2px"
+    // elmnt.style.borderStyle = "solid"
+    // elmnt.style.borderColor = "aqua"
+    elmnt.style.borderBottomWidth = "0px"
 }
